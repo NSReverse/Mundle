329c6198937084fdf65029cc1bb2b45f1014c6c4
Robert Brown <brownrc@live.com>
2017-07-09 02:04:38 -0500

    feat: Added UI for MainActivity and prepared for development of MainActivity.

--- app/src/main/java/net/nsreverse/mundle/ui/MainActivity.java
+++ app/src/main/java/net/nsreverse/mundle/ui/MainActivity.java
@@ -2,8 +2,12 @@ package net.nsreverse.mundle.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
 import net.nsreverse.mundle.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Class MainActivity -> AppCompatActivity -
@@ -15,9 +19,18 @@ import net.nsreverse.mundle.R;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String KEY_SESSION_ID = "session";
+    private static final int LOGIN_REQUEST_CODE = 101;
+    private static final int DELETE_REQUEST_CODE = 102;
+
+    @BindView(R.id.card_view_my_classes) private CardView classroomsCardView;
+    @BindView(R.id.card_view_my_notes) private CardView notesCardView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
