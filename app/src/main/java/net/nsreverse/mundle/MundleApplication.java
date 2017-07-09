52a1f9ee6bf574d44a6d6001a6ef2910cbbc4ef4
Robert Brown <brownrc@live.com>
2017-07-08 19:58:20 -0500

    feature: Adding Application subclass.
    This new Application subclass will provide a way to perform post-launch operations before the app's MainActivity is needed. This class will also provide a way for the app to house global variables like standard log enabling.

---
+++ app/src/main/java/net/nsreverse/mundle/MundleApplication.java
@@ -0,0 +1,47 @@
+package net.nsreverse.mundle;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+import net.nsreverse.mundle.data.ServerConfiguration;
+
+/**
+ * MundleApplication -
+ *
+ * This class provides any preconfiguration required for the app as well as houses
+ * app-wide global variables.
+ *
+ * @author Robert
+ * Created 7/8/2017.
+ */
+public class MundleApplication extends Application {
+    public static final boolean isLoggingEnabled = true;
+
+    /**
+     * Overridden Method onCreate -
+     *
+     * This method handles any post-launch configuration that the app needs in order to run.
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.enableLocalDatastore(this);
+
+        Parse.Configuration configuration = new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId(ServerConfiguration.Parse.getApplicationId())
+                .clientKey(ServerConfiguration.Parse.getMasterKey())
+                .server(ServerConfiguration.Parse.getServerAddress())
+                .build();
+
+        Parse.initialize(configuration);
+
+        ParseACL acl = new ParseACL();
+        acl.setPublicReadAccess(true);
+        acl.setPublicWriteAccess(true);
+
+        ParseACL.setDefaultACL(acl, true);
+    }
+}
