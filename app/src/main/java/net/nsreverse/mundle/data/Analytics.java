a6c6f015e4a38fd02f11d0beeccbe917433e4c92
Robert Brown <brownrc@live.com>
2017-07-18 06:36:05 -0500

    doc: Document data.Analytics

--- app/src/main/java/net/nsreverse/mundle/data/Analytics.java
+++ app/src/main/java/net/nsreverse/mundle/data/Analytics.java
@@ -6,18 +6,45 @@ import android.os.Bundle;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 /**
- * Created by Robert on 7/9/2017.
+ * Analytics -
+ *
+ * This class provides static convenience methods for logging with Firebase Analytics.
+ *
+ * @author Robert
+ * Created on 7/9/2017.
  */
-
 public class Analytics {
+
+    /**
+     * firebaseLogAppOpen(Context) -
+     *
+     * This method logs to Firebase Analytics when the app opens.
+     *
+     * @param context The Context required to get the FirebaseAnalytics instance.
+     */
     public static void firebaseLogAppOpen(Context context) {
         FirebaseAnalytics.getInstance(context).logEvent(FirebaseAnalytics.Event.APP_OPEN, null);
     }
 
+    /**
+     * firebaseLogUserLogin(Context) -
+     *
+     * This method logs to Firebase Analytics when the app logs a user in.
+     *
+     * @param context The Context required to get the FirebaseAnalytics instance.
+     */
     public static void firebaseLogUserLogin(Context context) {
         FirebaseAnalytics.getInstance(context).logEvent(FirebaseAnalytics.Event.LOGIN, null);
     }
 
+    /**
+     * firebaseLogNewUserType(Context) -
+     *
+     * This method logs to Firebase Analytics when the app registers and logs a user in under a
+     * new group type (teacher or student).
+     *
+     * @param context The Context required to get the FirebaseAnalytics instance.
+     */
     public static void firebaseLogNewUserType(Context context, boolean isTeacher) {
         String group = "student";
 
