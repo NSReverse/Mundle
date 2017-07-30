deeead8d9e0759afd9c57136f61950ce7103e16d
Robert Brown <brownrc@live.com>
2017-07-29 23:26:01 -0500

    doc: Document FCMAsyncTask.

--- app/src/main/java/net/nsreverse/mundle/data/notifier/FCMAsyncTask.java
+++ app/src/main/java/net/nsreverse/mundle/data/notifier/FCMAsyncTask.java
@@ -2,6 +2,7 @@ package net.nsreverse.mundle.data.notifier;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import net.nsreverse.mundle.MundleApplication;
@@ -12,17 +13,34 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
- * Created by Robert on 7/21/2017.
+ * FCMAsyncTask -
+ *
+ * This class pushes notifications through specific channels in the background.
+ *
+ * @author Robert
+ * Created on 7/21/2017.
  */
 public class FCMAsyncTask extends AsyncTask<String, Void, Boolean> {
     private static final String TAG = FCMAsyncTask.class.getSimpleName();
     private Delegate delegate;
 
+    /**
+     * Interface Delegate -
+     *
+     * This interface is used to give callbacks to implementing objects.
+     */
     public interface Delegate {
         void fcmMessagePosted(boolean success);
     }
 
-    public FCMAsyncTask(Context context) {
+    /**
+     * Constructor FCMAsyncTask(Context) -
+     *
+     * This is the main constructor for setting up a FCMAsyncTask.
+     *
+     * @param context The Context of the object implementing the Delegate interface. Can be null.
+     */
+    public FCMAsyncTask(@Nullable Context context) {
         if (context instanceof Delegate) {
             delegate = (Delegate)context;
         }
@@ -33,6 +51,14 @@ public class FCMAsyncTask extends AsyncTask<String, Void, Boolean> {
         super.onPreExecute();
     }
 
+    /**
+     * doInBackground(String...) -
+     *
+     * This method handles the background operations to push a notification.
+     *
+     * @param strings Variadic arguments needed to push a notification.
+     * @return A Boolean representing success of pushing a notification.
+     */
     @Override
     protected Boolean doInBackground(String... strings) {
         String message = strings[0];
@@ -67,6 +93,13 @@ public class FCMAsyncTask extends AsyncTask<String, Void, Boolean> {
         return result;
     }
 
+    /**
+     * onPostExecute(Boolean) -
+     *
+     * This method performs the post-execution callbacks to the Delegate-implementing object.
+     *
+     * @param success A Boolean representing if the push notification was sent.
+     */
     @Override
     protected void onPostExecute(Boolean success) {
         if (success) {
