cd6bc763697f35a3c7a78fbaa266f922f7213913
Robert Brown <brownrc@live.com>
2017-08-04 05:25:26 -0500

    doc: Document NotesAdapter

--- app/src/main/java/net/nsreverse/mundle/data/UserDefaults.java
+++ app/src/main/java/net/nsreverse/mundle/data/UserDefaults.java
@@ -16,6 +16,14 @@ public class UserDefaults {
     private static final String KEY_DEFAULT_TEACHER = "DEFAULT_TEACHER";
     private static final String KEY_DEFAULT_FCM_TOKEN = "DEFAULT_FCM_TOKEN";
 
+    /**
+     * setDefaultUsername(Context, String) -
+     *
+     * This method sets the new login username so that it is available upon next startup.
+     *
+     * @param context A Context to pass to SharedPreferences.
+     * @param username A String that represents the new default String username.
+     */
     public static void setDefaultUsername(Context context, String username) {
         SharedPreferences preferences = context.getSharedPreferences("ParseMundle", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
@@ -23,11 +31,27 @@ public class UserDefaults {
         editor.commit();
     }
 
+    /**
+     * getDefaultUsername(Context) -
+     *
+     * This method gets the last used username.
+     *
+     * @param context A Context to pass to SharedPreferences.
+     * @return A String representing the last known used username.
+     */
     public static String getDefaultUsername(Context context) {
         SharedPreferences preferences = context.getSharedPreferences("ParseMundle", Context.MODE_PRIVATE);
         return preferences.getString(KEY_DEFAULT_USERNAME, "");
     }
 
+    /**
+     * setDefaultTeacher(Context, boolean) -
+     *
+     * This method sets the new flag if the last logged in user was an instructor.
+     *
+     * @param context A Context to pass to SharedPreferences.
+     * @param isTeacher A boolean representing if the last user was an instructor.
+     */
     public static void setDefaultTeacher(Context context, boolean isTeacher) {
         SharedPreferences preferences = context.getSharedPreferences("ParseMundle", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
@@ -35,11 +59,24 @@ public class UserDefaults {
         editor.commit();
     }
 
+    /**
+     * getDefaultTeacher(Context) -
+     *
+     * This method gets a boolean representing if the last known login was an instructor.
+     *
+     * @param context A Context to pass to SharedPreferences.
+     * @return A boolean representing if the last known user was an instructor.
+     */
     public static boolean getDefaultTeacher(Context context) {
         SharedPreferences preferences = context.getSharedPreferences("ParseMundle", Context.MODE_PRIVATE);
         return preferences.getBoolean(KEY_DEFAULT_TEACHER, false);
     }
 
+    /**
+     *
+     * @param context
+     * @param token
+     */
     public static void setDefaultFirebaseToken(Context context, String token) {
         SharedPreferences preferences = context.getSharedPreferences("ParseMundle", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
--- app/src/main/java/net/nsreverse/mundle/ui/adapters/NotesAdapter.java
+++ app/src/main/java/net/nsreverse/mundle/ui/adapters/NotesAdapter.java
@@ -19,15 +19,29 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
- * Created by Robert on 7/21/2017.
+ * NotesAdapter -
+ *
+ * This class adapts data to a RecyclerView which shows a list of Notes that a user has
+ * created.
+ *
+ * @author Robert
+ * Created on 7/21/2017.
  */
-
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
 
     private Delegate delegate;
     private List<ParseObject> dataSource;
     private List<Note> loadedNotesDataSource;
 
+    /**
+     * onCreateViewHolder(ViewGroup, int) -
+     *
+     * This method inflates a new NoteViewHolder for displaying data.
+     *
+     * @param parent The parent container where this ViewHolder will be located.
+     * @param viewType Unused.
+     * @return A new NoteViewHolder for displaying data.
+     */
     @Override
     public NoteViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
@@ -36,12 +50,27 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHold
         return new NoteViewHolder(view);
     }
 
+    /**
+     * onBindViewHolder(NoteViewHolder, int) -
+     *
+     * This method binds the data source to the NoteViewHolder at the specified position.
+     *
+     * @param holder A NoteViewHolder to set data to.
+     * @param position An int representing the position of the data source.
+     */
     @Override
     public void onBindViewHolder(NoteViewHolder holder, int position) {
         holder.titleTextView.setText(loadedNotesDataSource.get(position).getTitle());
         holder.subtitleTextView.setText(loadedNotesDataSource.get(position).getContent());
     }
 
+    /**
+     * getItemCount() -
+     *
+     * This method gets the size of the data source.
+     *
+     * @return An int representing the size of the data source.
+     */
     @Override
     public int getItemCount() {
         if (loadedNotesDataSource == null) {
@@ -51,6 +80,15 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHold
         return loadedNotesDataSource.size();
     }
 
+    /**
+     * setDataSource(Context, List [-> ParseObject]) -
+     *
+     * This method sets the data source and the delegate of this adapter.
+     *
+     * @param context The Context (implementing Delegate if needed) to get resources and set
+     *                callbacks.
+     * @param dataSource A List [-> ParseObject] to use as the data source.
+     */
     public void setDataSource(Context context, List<ParseObject> dataSource) {
         if (context instanceof NotesAdapter.Delegate) {
             delegate = (NotesAdapter.Delegate)context;
@@ -59,26 +97,59 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHold
         this.dataSource = dataSource;
     }
 
+    /**
+     * setLoadedNotesDataSource(List [-> Notes]) -
+     *
+     * This method sets the data retrieved from the ContentProvider and a Loader (Udacity's
+     * specification).
+     *
+     * @param notesDataSource A List [-> Note] containing Notes to set as the data source.
+     */
     public void setLoadedNotesDataSource(List<Note> notesDataSource) {
         loadedNotesDataSource = notesDataSource;
         notifyDataSetChanged();
     }
 
+    /**
+     * Interface Delegate -
+     *
+     * This Interface allows an Object that implements it to recieve callbacks when registered
+     * as this Adapter's delegate.
+     */
     public interface Delegate {
         void adapterItemClicked(int position, ParseObject selectedObject);
     }
 
+    /**
+     * itemClicked(int) -
+     *
+     * This method uses the Delegate as a callback when an item is selected.
+     *
+     * @param position An int representing the selected position.
+     */
     private void itemClicked(int position) {
         if (delegate != null) delegate.adapterItemClicked(position, dataSource.get(position));
         System.out.println("A note has been clicked.");
     }
 
+    /**
+     * NoteViewHolder -
+     *
+     * This ViewHolder defines the layout of the View that will display data from the data source.
+     */
     class NoteViewHolder extends RecyclerView.ViewHolder
                          implements View.OnClickListener {
 
         @BindView(R.id.text_view_note_title) TextView titleTextView;
         @BindView(R.id.text_view_note_date) TextView subtitleTextView;
 
+        /**
+         * Constructor NoteViewHolder(View) -
+         *
+         * This is the main constructor for this class to create a new NoteViewHolder.
+         *
+         * @param itemView A View to display data.
+         */
         NoteViewHolder(View itemView) {
             super(itemView);
 
@@ -87,6 +158,13 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHold
             itemView.setOnClickListener(this);
         }
 
+        /**
+         * onClick(View) -
+         *
+         * This method handles whenever an item is selected in this View.
+         *
+         * @param view The View that was selected. Unused.
+         */
         @Override
         public void onClick(View view) {
             itemClicked(getAdapterPosition());
